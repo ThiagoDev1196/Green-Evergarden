519413c75113f3d77a87d2d422a703cb04fd89ae
Thiago <thiagomarcial1109@gmail.com>
2025-01-23 23:28:02 -0300

    First commit - Pushing the proyect to GitHub

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+
+    <header> 
+      {/* Componente del nav */}
+    </header>
+
+    <main>
+      {/* Componente del hero img */}
+      {/* Componente about us  */}
+      {/* Componente productos destacados  */}
+      {/* Componente de beneficios */}
+      {/* Componente de incentivo */}
+
+    </main>
+
+    <footer>
+      {/* Componente del footer  */}
+    </footer>
+    
+    </>
+  )
+}
+
+export default App
