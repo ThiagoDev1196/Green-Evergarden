7939d5ecd3d53faa6c4c7eed971a636a7deebfb7
Thiago <thiagomarcial1109@gmail.com>
2026-01-12 21:44:56 -0300

    fixing images for responsive design

--- src/components/incentive.jsx
+++ src/components/incentive.jsx
@@ -8,10 +8,10 @@ function Incentive() {
                 <img src="/incentive-img.jpg" className="h-[450px] w-full object-cover"/>
             </div>
 
-            <div className="incentive__text flex-[1] relative bg-[url('/incentive3.jpg')] bg-cover bg-center text-black p-10 md:bg-none">
+            <div className="incentive__text flex-[1] relative bg-[url('/incentive-img.jpg')] bg-cover bg-center text-black p-10 md:bg-none">
              <div className="absolute inset-0 bg-black opacity-50 z-0 md:hidden"></div> 
                 <p className="bg-[#ffffffE6] md:bg-emerald-800 md:text-white text-center text-2xl font-bold p-6 z-10 relative">Why green products?</p>
-                <p className="p-6 bg-[#ffffffe6] md:text-md  md:bg-emerald-800 md:text-white z-10 relative ">Every choice you make as a consumer impacts the environment. By choosing eco-friendly products, you help reduce pollution, minimize waste, and support sustainable practices. Green products use natural ingredients, biodegradable packaging, and ethical sourcing, making them safer for both people and the planet. Switching to sustainable alternatives means cleaner oceans, fresher air, and a healthier future for all. Small actions lead to big change—buy green and make a difference today! ♻️</p>
+                <p className="p-6 bg-[#ffffffe6] md:text-md  md:bg-emerald-800 md:text-white z-10 relative">Every choice you make as a consumer impacts the environment. By choosing eco-friendly products, you help reduce pollution, minimize waste, and support sustainable practices. Green products use natural ingredients, biodegradable packaging, and ethical sourcing, making them safer for both people and the planet. Switching to sustainable alternatives means cleaner oceans, fresher air, and a healthier future for all. Small actions lead to big change—buy green and make a difference today! ♻️</p>
             </div>
         </div>
     );
